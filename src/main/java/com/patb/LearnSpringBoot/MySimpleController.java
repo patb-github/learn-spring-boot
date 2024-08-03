0a9bda01105cea676723465a1e0e652d8d7fcaf8
Pat <patb.software@gmail.com>
2024-08-03 16:41:23 +0700

    Add test and sum endpoints

--- src/main/java/com/patb/LearnSpringBoot/MySimpleController.java
+++ src/main/java/com/patb/LearnSpringBoot/MySimpleController.java
@@ -1,13 +1,28 @@
 package com.patb.LearnSpringBoot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class MySimpleController {
 
-    @RequestMapping("/")
+    @RequestMapping(value={"/", "/home"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String home() {
         return "Hello, World!";
     }
+
+    @GetMapping("/test")
+    public String secondHome() {
+        return "Hello, World 2!";
+    }
+    
+    @GetMapping("/sum")
+    public int sum(@RequestParam("a") int a, @RequestParam("b") int b) {
+        return a + b;
+    }
+    
 }
