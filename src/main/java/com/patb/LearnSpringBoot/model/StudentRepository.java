1a53becac22e2cacc6f698d74eaa52d698eebf89
Pat <patb.software@gmail.com>
2024-08-04 11:21:32 +0700

    Create Student entity and StudentRepository interface

---
+++ src/main/java/com/patb/LearnSpringBoot/model/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.patb.LearnSpringBoot.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
